f8518c7a1ff5933690ba6c81729e3402bfdeda52
Jack <jackpinkerton@live.com.au>
2022-01-14 10:48:39 +1100

    metadata and first blog post updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Pink.ai`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Jack Pinkerton`,
+      summary: `I watch sport and occasionally write some code`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A Tech and Sport Blog`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
       twitter: `kylemathews`,
